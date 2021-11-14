0ad4da300004c7922d0f9ce5be021561ef3c6ecb
Lynn <clhuang224@gmail.com>
2021-11-15 00:59:24 +0800

    chore: deal with deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 export default defineConfig({
+  base: '/travel_2021/',
   plugins: [
     vue(),
     Components({
